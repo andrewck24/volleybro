3343c1ad6b173be331e6a11b846b1887680d4223
AndrewCK24 <andrew50412@gmail.com>
2025-04-29 16:08:28 +0800

    feat(record): integrate accordion for sets list and enhance UI interactions

--- src/components/match/sets/list.tsx
+++ src/components/match/sets/list.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { Figure } from "@/components/custom/stats/figures";
 import { SetOptions } from "@/components/record/set-options";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import { Dialog } from "@/components/ui/dialog";
 import type { Set } from "@/entities/record";
@@ -10,7 +16,7 @@ import { recordActions } from "@/lib/features/record/record-slice";
 import { useAppDispatch } from "@/lib/redux/hooks";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { RiAddLine, RiMoreLine } from "react-icons/ri";
+import { RiAddLine, RiArrowRightLine, RiMoreLine } from "react-icons/ri";
 
 export const SetsList = ({ recordId }: { recordId: string }) => {
   const { record } = useRecord(recordId);
@@ -24,15 +30,17 @@ export const SetsList = ({ recordId }: { recordId: string }) => {
 
   return (
     <>
-      {record?.sets.map((set, index) => (
-        <SetInfo
-          key={index}
-          recordId={recordId}
-          set={set}
-          setIndex={index}
-          handleOptionsOpen={handleOptionsOpen}
-        />
-      ))}
+      <Accordion type="single" collapsible className="flex flex-col gap-2">
+        {record?.sets.map((set, index) => (
+          <SetItem
+            key={index}
+            recordId={recordId}
+            set={set}
+            setIndex={index}
+            handleOptionsOpen={handleOptionsOpen}
+          />
+        ))}
+      </Accordion>
       <Button size="lg" onClick={() => handleOptionsOpen(record.sets.length)}>
         <RiAddLine />
         新增一局
@@ -44,7 +52,7 @@ export const SetsList = ({ recordId }: { recordId: string }) => {
   );
 };
 
-const SetInfo = ({
+const SetItem = ({
   recordId,
   set,
   setIndex,
@@ -59,21 +67,34 @@ const SetInfo = ({
   const rally = getPreviousRally(set.entries, set.entries.length);
 
   return (
-    <div className="flex flex-1 items-center justify-center gap-2 rounded border hover:bg-accent/80">
-      <div
-        className="flex flex-1 items-center justify-center gap-2 py-4 pl-4"
-        onClick={() => router.push(`/record/${recordId}?si=${setIndex}`)}
-      >
+    <AccordionItem
+      value={setIndex.toString()}
+      className="rounded-md border last:border"
+    >
+      <AccordionTrigger className="flex flex-1 items-center justify-center gap-2 border-0 p-4 hover:bg-accent/80 [&>svg]:size-6">
         <Figure value={rally ? rally.home.score : 0} />
-        <span className="flex flex-1 justify-center text-lg font-semibold">{`第 ${setIndex + 1} 局`}</span>
+        <span className="flex flex-1 justify-center text-lg">{`第 ${setIndex + 1} 局`}</span>
         <Figure value={rally ? rally.away.score : 0} />
-      </div>
-      <div
-        className="flex items-center justify-center gap-2 py-4 pr-4"
-        onClick={() => handleOptionsOpen(setIndex)}
-      >
-        <RiMoreLine className="size-6" />
-      </div>
-    </div>
+      </AccordionTrigger>
+      <AccordionContent className="p-2">
+        <div className="grid w-full grid-cols-2 gap-2">
+          <Button
+            variant="outline"
+            size="lg"
+            onClick={() => handleOptionsOpen(setIndex)}
+          >
+            <RiMoreLine className="size-6" />
+            檢視設定
+          </Button>
+          <Button
+            size="lg"
+            onClick={() => router.push(`/record/${recordId}?si=${setIndex}`)}
+          >
+            進入比賽
+            <RiArrowRightLine className="size-6" />
+          </Button>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
   );
 };
