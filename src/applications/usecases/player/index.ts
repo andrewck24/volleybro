736510ae4640aa6cf315f10e9683f8dd09fc5f7c
andrewck24 <andrewck24@gmail.com>
2025-12-20 17:09:41 +0800

    fix(player): export use case implementations from index for DI container
    Fix DI container binding error by exporting use case implementation classes
    alongside their interfaces. Use 'export type' for interfaces to satisfy
    TypeScript isolatedModules requirement.
    
    - Export all 6 use case implementations
    - Use 'export type' for interface exports
    - Fixes runtime TypeError in inversify.config.ts
    
    Status: 365 tests passing, dev server now starts correctly

--- src/applications/usecases/player/index.ts
+++ src/applications/usecases/player/index.ts
@@ -2,9 +2,18 @@
  * Player Use Case Exports
  */
 
-export { ICreateInvitationUseCase } from './create-invitation.usecase.interface';
-export { IGetUserPlayersUseCase } from './get-user-players.usecase.interface';
-export { IAcceptInvitationUseCase } from './accept-invitation.usecase.interface';
-export { IRejectInvitationUseCase } from './reject-invitation.usecase.interface';
-export { IGetTeamPlayersUseCase } from './get-team-players.usecase.interface';
-export { IGetPlayerUseCase } from './get-player.usecase.interface';
+// Interfaces
+export type { ICreateInvitationUseCase } from './create-invitation.usecase.interface';
+export type { IGetUserPlayersUseCase } from './get-user-players.usecase.interface';
+export type { IAcceptInvitationUseCase } from './accept-invitation.usecase.interface';
+export type { IRejectInvitationUseCase } from './reject-invitation.usecase.interface';
+export type { IGetTeamPlayersUseCase } from './get-team-players.usecase.interface';
+export type { IGetPlayerUseCase } from './get-player.usecase.interface';
+
+// Implementations
+export { CreateInvitationUseCase } from './create-invitation.usecase';
+export { GetUserPlayersUseCase } from './get-user-players.usecase';
+export { AcceptInvitationUseCase } from './accept-invitation.usecase';
+export { RejectInvitationUseCase } from './reject-invitation.usecase';
+export { GetTeamPlayersUseCase } from './get-team-players.usecase';
+export { GetPlayerUseCase } from './get-player.usecase';
